755e4ae0dce74093d9a8585f6d98955825d04aa1
SimmaLimma <simonmaartensson@gmail.com>
2018-11-08 16:44:16 +0100

    Trying to figure out why CSS does not work

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-from forms import SearchForm
 
 
 # Youtube tutorial
@@ -12,16 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
-
-@app.route('/about')
-def about():
-    return render_template('about.html')
-
-@app.route('/search')
-def search():
-    form = SearchForm()
-    return render_template('search.html', title='Search', form=form)
+    return render_template('template.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
